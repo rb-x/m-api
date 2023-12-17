d2eb46a4e9ff7039c5c9c7ab0e55af9253fd6d07
Riadh B <riadh.bch@outlook.fr>
2023-12-17 03:00:12 +0100

    listing devices ok

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express } from "express";
+import swaggerUi from 'swagger-ui-express';
+import { RegisterRoutes } from './routes/routes';
 
-dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+RegisterRoutes(app);
+
+const swaggerDocument = require('./swagger/swagger.json');
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
